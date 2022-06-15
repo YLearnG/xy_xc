b571fd1add0aff214f5a9f43ff1e847c2afd075a
yanglong <1060794017@qq.com>
2022-06-15 11:05:12 +0800

    build  the  project
    Signed-off-by: yanglong <1060794017@qq.com>

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,8 @@
 import request from '@/utils/request'
 
+/*
+* 登录接口封装
+* */
 export function login(data) {
   return request({
     url: '',
@@ -8,10 +11,31 @@ export function login(data) {
   })
 }
 
-export function logout(data) {
+/*
+* 获取用户资料的接口
+* */
+export function getUsrInfo() {
   return request({
     url: '',
-    methods: '',
-    data
+    methods: ''
+  })
+}
+
+/*
+*根据用户id获取用户详情
+* */
+export function getUserDatailById() {
+  return request({
+    url: '',
+  })
+}
+
+/*
+* 退出的接口封装
+* */
+export function logOut() {
+  return request({
+    url: '',
+    methods: ''
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,4 @@
-// 配置请求的拦截器
-import axios from 'axios'
+import axios from "axios";
 
 const service = axios.create()
 
